b50a782d759e646252e75e9980354fbc87603b2a
python240419 <51232248+python240419@users.noreply.github.com>
2019-08-02 10:51:55 +0300

    Update oop_data.py

--- oop_data.py
+++ oop_data.py
@@ -15,7 +15,28 @@ print(mac.color)
 # create Person class
 # func: sayHello()
 # data: name , __init__  parameter
+# data: age , __init__  parameter
 # *etgar: sayMyName() - print
 #       current person name
 # create 2 persons
 # set name default value 'Incognito'
+class Person():
+    def __init__(self,
+                 time,
+                 name = 'Incognito'
+                 , age = 0):
+        self.__time = time
+        self.name = name
+        self.age = age
+    def sayMyName(self):
+        print(f'name = {self.name}')
+    def getTime(self):
+        return self.__time + ' GMT+3'
+
+dana = Person('13:00', 'Dana', 33)
+print(dana.name, dana.age)
+dana.sayMyName()
+print(dana.getTime())
+
+
+
