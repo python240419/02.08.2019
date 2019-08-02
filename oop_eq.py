0fcf69ced06bd152bcbca163de4e684e70394bdb
python240419 <51232248+python240419@users.noreply.github.com>
2019-08-02 11:52:44 +0300

    Create oop_eq.py

---
+++ oop_eq.py
@@ -0,0 +1,37 @@
+
+# class Point
+# __init__(self, x, y)
+
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    # str - 1st priority
+    # repr - developers
+    def __repr__(self):
+        return f'Point({self.x}, ' \
+            f'{self.y})'
+    def __str__(self):
+        return f'Point x={self.x} y={self.y}'
+    def __eq__(self, other):
+        if self.x == other.x and\
+            self.y == other.y:
+            return True
+        else:
+            return False
+
+p1 = Point(3.5, 8.8)
+#print(p1.__repr__())
+p2 = Point(5.9, 10.1) # 50
+#print(p2)
+p3 = Point(5.9, 10.1) # 60
+#p2 = p3 # p2 = 60
+print(p2 == p3)
+#print(p2 == (3, 4))
+
+# MobilePhone
+# __init__ (self, brand, model)
+#   data - brand, model
+# __rerp__, __str__, __eq__
+
