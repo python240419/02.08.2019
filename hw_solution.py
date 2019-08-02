838ffd040c915780715d169a419657fa9d1ed8dd
python240419 <51232248+python240419@users.noreply.github.com>
2019-08-02 09:54:59 +0300

    Create hw_solution.py

---
+++ hw_solution.py
@@ -0,0 +1,50 @@
+'''
+# fucntion
+def myPrint():
+    pass
+
+class Computer():
+    def __init__(self):
+        print("new computer was born")
+    def turnOn(self):
+        print("turning on...")
+        self.__secretMethod()
+    def __secretMethod(self):
+        print("shhhhhh")
+
+print('hello')   # function
+lenovo = Computer()
+lenovo.turnOn() # method
+
+'''
+
+def sortToTuple(*args):
+    return tuple(sorted(args))
+
+print(sortToTuple(7,2,-3, 100))
+
+def compareD(d, **kwargs):
+    d == kwargs
+
+compareD({'name' : 'itay'},
+         name = 'itay')
+
+#{ name : 'itay'} == {name : 'itay'}
+
+class SuperHero():
+    def __init__(self):
+        print("new hero was born")
+    def fly(self):
+        print("flying....")
+    def climb(self):
+        print("climbing....")
+    def forceSpeed(self):
+        print("force Speed....")
+        self.__heroResting()
+    def __heroResting(self):
+        print("resting....")
+superMan = SuperHero()
+superMan.fly()
+superMan.__heroResting()
+superMan.climb()
+superMan.forceSpeed()
